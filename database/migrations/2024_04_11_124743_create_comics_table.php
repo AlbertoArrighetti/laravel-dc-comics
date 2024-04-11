a3067a36bb52db8e035d73564ebe4441e0de5ebc
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-11 14:54:43 +0200

    feat comics

---
+++ database/migrations/2024_04_11_124743_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 10);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 100);
+
+            $table->array('artists');
+            $table->array('writers');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
