a29ead8fca8161ef8480ae4f86ddda15ea2410e6
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-11 15:56:19 +0200

    feat comic index

---
+++ resources/views/comic/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container py-5">
+    <h1 class="mb-3">Comics List</h1>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach ($comics as $comic)
+          <tr>
+
+              <td>{{$comic->title}}</td>
+            
+            <td><a href="">visualizza dettagli</a></td>
+            
+        </tr>
+        @endforeach
+        </tbody>
+      </table>
+</div>
+@endsection
