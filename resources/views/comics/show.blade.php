0b0b3af5d4cdb37bfd14c43eebb206a0e81e1c8d
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-04-15 16:21:26 +0200

    feat: style added

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,28 +7,28 @@
     <div class="card mb-3">
         <div class="row g-0">
 
-          <div class="col-md-4">
+          <div class="col-md-4 p-4">
             <img src="{{$comic->thumb}}" class="img-fluid rounded-start" alt="{{$comic->title}}">
           </div>
           
           <div class="col-md-8">
 
-            <div class="card-body">
+            <div class="card-body my-2 ">
 
-              <h5 class="card-title mb-5 text-center ">{{$comic->title}}</h5>
+              <h5 class="card-title mb-4 fs-2 text-center ">{{$comic->title}}</h5>
 
               <p class="card-text">{{$comic->description}}</p>
-              <p class="card-text"><small class="text-body-secondary">{{$comic->price}}</small></p>
-              <p class="card-text">Type: <small class="text-body-secondary">{{$comic->type}}</small></p>
-              <p class="card-text">Series: <small class="text-body-secondary">{{$comic->series}}</small></p>
+              <p class="card-text fw-bold pb-3 ">{{$comic->price}}</p>
+              <p class="card-text fw-bold ">Type: <small class="text-body-secondary">{{$comic->type}}</small></p>
+              <p class="card-text fw-bold ">Series: <small class="text-body-secondary">{{$comic->series}}</small></p>
            
 
-              <div class="card-text px-3 mb-3">Written by: 
-                <small class="text-body-secondary ">{{$comic->writers}}</small>
+              <div class="card-text mb-3 fw-bold ">Written by: 
+                <small class="text-body-secondary">{{$comic->writers}}</small>
               </div>
               
-              <div class="card-text px-3">Art by: 
-                <small class="text-body-secondary ">{{$comic->artists}}</small>
+              <div class="card-text fw-bold ">Art by: 
+                <small class="text-body-secondary">{{$comic->artists}}</small>
               </div>
             </div>
 
